be11f3849043c07d5e34e66e9a126161a58c9ae4
AIwinski <artur.iwinski@gmail.com>
2019-11-21 20:49:02 +0100

    init repo; add some backend features

---
+++ backend/routes/users.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+
+const User = mongoose.model("User");
+
+router.post("/register", (req, res, next) => {
+    User.find({
+        email: req.body.email
+    })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(400).json({
+                    message: "Mail already exists"
+                });
+            } else {
+                if (!validator.isEmail(req.body.email)) {
+                    return res.status(400).json({
+                        message: "Invalid email"
+                    });
+                }
+                if (validator.isEmpty(req.body.password) || req.body.password.length < 6) {
+                    return res.status(400).json({
+                        message: "Invalid password"
+                    });
+                }
+
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err,
+                            message: "hashing error"
+                        });
+                    } else {
+                        let user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                return res.status(201).json({
+                                    message: "User created successfully.",
+                                    user: result
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                return res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            return res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.post("/login", (req, res, next) => {
+    User.findOne({
+        email: req.body.email
+    })
+        .exec()
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: "Auth failed1"
+                });
+            }
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: "Auth failed2"
+                    });
+                } else {
+                    if (result === false) {
+                        return res.status(401).json({
+                            message: "Auth failed3"
+                        });
+                    } else {
+                        const token = jwt.sign(
+                            {
+                                userId: user._id
+                            },
+                            "JWT_KEY", //process.env.JWT_KEY,
+                            {
+                                expiresIn: "1d"
+                            }
+                        );
+                        userInfo = {
+                            _id: user._id,
+                            email: user.email
+                        };
+                        return res.status(200).json({
+                            message: "Auth successful",
+                            token: token,
+                            user: userInfo
+                        });
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+module.exports = router;
