f9aa8d24fac3e9427a36493ec56683650a00d59e
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2019-11-22 16:00:17 +0100

    Added login & register buttons

--- paragon_app/App.tsx
+++ paragon_app/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.headerView}>
         <Text style={styles.headerText}>Paragon App</Text>
+      </View>
+      <View style={styles.bodyView}>
+        <View style={styles.buttomView}>
+          <Button title="Login"/>
+          </View>
+        <View style={styles.buttomView}>
+          <Button title="Register"/>
+          </View>
       </View>
     </View>
   );
@@ -29,5 +37,19 @@ const styles = StyleSheet.create({
   },
   headerText: {
     fontSize: 32,
+  },
+  bodyView:{
+    borderRadius: 20,
+    paddingTop: 50,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  buttomView: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10
   }
+  
 });
