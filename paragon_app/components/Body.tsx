8a8b6acb1d35e9a00ccc14c6317adb7750dac08e
Bogost <bogumilostapiuk@gmail.com>
2019-11-29 15:18:03 +0100

    adding states and architectural refactor

--- paragon_app/components/Body.tsx
+++ paragon_app/components/Body.tsx
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import { StyleSheet, Button, View } from 'react-native';
+import { StyleSheet, Button, View, Text } from 'react-native';
+
+//enum loginStateE = {Login, Register, None};
 
 export class Body extends Component {
-  render() {
-    return (
-        <View style={styles.bodyView}>
-            <View style={styles.buttomView}>
-                <Button title="Login"/>
-            </View>
-            <View style={styles.buttomView}>
-                <Button title="Register"/>
-            </View>
-        </View>
-    );
-  }
+    state = { logged: false,
+        loginState: "None",
+        };
+
+    render() {
+        let body;
+        if(!this.state.logged)
+        {
+            if(this.state.loginState==="None")
+            {
+                return( <View style={styles.bodyView}>
+                            <View style={styles.buttomView}>
+                                <Button title="Login"/>
+                            </View>
+                            <View style={styles.buttomView}>
+                                <Button title="Register"/>
+                            </View>
+                        </View>);
+            }
+            else if(this.state.loginState==="Login")
+            {
+                return (
+                    <Text>Developing...</Text>
+                );
+            }
+            else if(this.state.loginState==="Register")
+            {
+                return (
+                    <Text>Developing...</Text>
+                );
+            }
+        }
+        else
+        {
+            return (
+                <Text>Logged</Text>
+            );
+        }
+        return (
+            <Text>error</Text>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
