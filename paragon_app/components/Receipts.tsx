9dc9e33e767c62662a7b5ea4d0424bfee4687465
AIwinski <artur.iwinski@gmail.com>
2020-01-17 14:40:53 +0100

    add receipt tsx

---
+++ paragon_app/components/Receipts.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { API_URL } from '../../api'
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+export class Receipt extends Component {
+
+    render() {
+        const imageUrl = API_URL + this.props.imageUrl;
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>{this.props.title}</Text>
+                <Text style={styles.description}>{this.props.description}</Text>
+                <Image source={{uri: imageUrl}} style={styles.image} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 4,
+        borderWidth: 1,
+        borderColor: '#d6d7da',
+        width: "100%"
+    },
+    title: {
+        fontSize: 19,
+        fontWeight: 'bold',
+    },
+    description: {
+        color: 'red',
+    },
+    image: {
+        width: 50,
+        height: 50
+    }
+});
